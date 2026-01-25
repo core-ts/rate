0e8c9a45555f29b5cc668dc0b2a10360da07d879
Duc Nguyen <minhduc140583@gmail.com>
2026-01-25 23:42:08 +0700

    Refactor RateService

--- src/index.ts
+++ src/index.ts
@@ -6,32 +6,32 @@ export * from './rate';
 // tslint:disable-next-line:max-classes-per-file
 export class RateService implements Rater<Rate> {
   constructor(
-    public repository: BaseRepository<Rate>,
-    public infoRepository: InfoRepository) {
+    protected rateRepository: BaseRepository<Rate>,
+    protected infoRepository: InfoRepository) {
     this.rate = this.rate.bind(this);
   }
   async rate(rate: Rate): Promise<number> {
     rate.time = new Date();
     const info = await this.infoRepository.exist(rate.id);
     if (!info) {
-      const r0 = await this.repository.create(rate, true);
-      return r0;
+      const res = await this.rateRepository.create(rate, true);
+      return res;
     }
-    const exist = await this.repository.load(rate.id, rate.author);
+    const exist = await this.rateRepository.load(rate.id, rate.author);
     if (!exist) {
-      const r1 = await this.repository.create(rate);
-      return r1;
+      const res = await this.rateRepository.create(rate);
+      return res;
     }
-    const sr: ShortRate = { review: exist.review, rate: exist.rate, time: exist.time };
+    const history: ShortRate = { review: exist.review, rate: exist.rate, time: exist.time };
     if (exist.histories && exist.histories.length > 0) {
-      const history = exist.histories;
-      history.push(sr);
-      rate.histories = history;
+      const histories = exist.histories;
+      histories.push(history);
+      rate.histories = histories;
     } else {
-      rate.histories = [sr];
+      rate.histories = [history];
     }
-    const res = await this.repository.update(rate, exist.rate);
-    return res;
+    const count = await this.rateRepository.update(rate, exist.rate);
+    return count;
   }
 }
 export function avg(n: number[]): number {
@@ -44,8 +44,8 @@ export function avg(n: number[]): number {
 // tslint:disable-next-line:max-classes-per-file
 export class RatesService implements Rater<Rates> {
   constructor(
-    public repository: BaseRepository<Rates>,
-    public infoRepository: InfoRepository) {
+    protected rateRepository: BaseRepository<Rates>,
+    protected infoRepository: InfoRepository) {
     this.rate = this.rate.bind(this);
   }
   async rate(rate: Rates): Promise<number> {
@@ -55,24 +55,24 @@ export class RatesService implements Rater<Rates> {
     }
     rate.time = new Date();
     if (!info) {
-      const r0 = await this.repository.create(rate, true);
-      return r0;
+      const res = await this.rateRepository.create(rate, true);
+      return res;
     }
-    const exist = await this.repository.load(rate.id, rate.author);
+    const exist = await this.rateRepository.load(rate.id, rate.author);
     if (!exist) {
-      const r1 = await this.repository.create(rate);
-      return r1;
+      const res = await this.rateRepository.create(rate);
+      return res;
     }
-    const sr: ShortRates = { review: exist.review, rates: exist.rates, time: exist.time };
+    const history: ShortRates = { review: exist.review, rates: exist.rates, time: exist.time };
     if (exist.histories && exist.histories.length > 0) {
-      const history = exist.histories;
-      history.push(sr);
-      rate.histories = history;
+      const histories = exist.histories;
+      histories.push(history);
+      rate.histories = histories;
     } else {
-      rate.histories = [sr];
+      rate.histories = [history];
     }
-    const res = await this.repository.update(rate, exist.rate);
-    return res;
+    const count = await this.rateRepository.update(rate, exist.rate);
+    return count;
   }
 }
 interface ErrorMessage {
