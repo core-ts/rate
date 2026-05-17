5113b6ab0616917decd209aabd91355c8f5466ff
Duc Nguyen <minhduc140583@gmail.com>
2026-05-17 11:46:16 +0700

    Combine into 1 file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,142 @@
-import { Attributes } from './core';
-import { BaseRepository, InfoRepository, Rate, Rater, Rates, ShortRate, ShortRates } from './rate';
+// core.ts
+export type DataType = 'ObjectId' | 'date' | 'datetime' | 'time'
+  | 'boolean' | 'number' | 'integer' | 'string' | 'text'
+  | 'object' | 'array' | 'binary'
+  | 'primitives' | 'booleans' | 'numbers' | 'integers' | 'strings' | 'dates' | 'datetimes' | 'times';
+export type Operator = "=" | "like" | "!=" | "<>" | ">" | ">=" | "<" | "<="
 
-export * from './rate';
+export interface Attribute {
+  field?: string;
+  column?: string;
+  type?: DataType;
+  required?: boolean;
+  operator?: Operator;
+  key?: boolean;
+  q?: boolean;
+  min?: number;
+  max?: number;
+  typeof?: Attributes;
+}
+export interface Attributes {
+  [key: string]: Attribute;
+}
+
+export interface Filter {
+  page?: number;
+  limit: number;
+  fields?: string[];
+  sort?: string;
+  q?: string;
+}
+
+// rate.ts
+export interface RateId {
+  id: string;
+  author: string;
+}
+export interface BaseRate {
+  author: string;
+  authorURL?: string;
+  name: string;
+  displayName: string;
+  anonymous: boolean;
+  rate: number;
+}
+export interface Rate extends BaseRate {
+  id: string;
+  time: Date;
+  review: string;
+  usefulCount: number;
+  replyCount: number;
+  histories?: ShortRate[];
+}
+export interface ShortRate {
+  rate: number;
+  time: Date;
+  review: string;
+}
+export interface RateFilter extends Filter {
+  id?: string;
+  author?: string;
+  rate: number;
+  time?: Date;
+  review?: string;
+  usefulCount?: number;
+  replyCount?: number;
+}
+export interface RateInfo {
+  id: string;
+  rate: number;
+  count: number;
+  score: number;
+}
+export interface ShortRates {
+  rates: number[];
+  time: Date;
+  review: string;
+}
+export interface Rates extends BaseRate {
+  id: string;
+  rates: number[];
+  time: Date;
+  review: string;
+  usefulCount: number;
+  replyCount: number;
+  histories?: ShortRates[];
+}
+export interface RatesFilter extends RateFilter {
+  rates?: number[];
+  rate1: number;
+  rate2: number;
+  rate3: number;
+  rate4: number;
+  rate5: number;
+  rate6: number;
+  rate7: number;
+  rate8: number;
+  rate9: number;
+  rate10: number;
+}
+export interface BaseRepository<R> {
+  create(rate: R, newInfo?: boolean): Promise<number>;
+  update(rate: R, oldRate: number): Promise<number>;
+  load(id: string, author: string): Promise<R | null>;
+}
+
+export interface Rater<R> {
+  rate(rate: R): Promise<number>;
+}
+
+export interface Info {
+  id: string;
+  rate: number;
+  rate1: number;
+  rate2: number;
+  rate3: number;
+  rate4: number;
+  rate5: number;
+  count: number;
+  score: number;
+}
+export interface Info10 {
+  id: string;
+  rate: number;
+  rate1: number;
+  rate2: number;
+  rate3: number;
+  rate4: number;
+  rate5: number;
+  rate6: number;
+  rate7: number;
+  rate8: number;
+  rate9: number;
+  rate10: number;
+  count: number;
+  score: number;
+}
+export interface InfoRepository {
+  exist(id: string, ctx?: any): Promise<boolean>;
+}
 
 // tslint:disable-next-line:max-classes-per-file
 export class RateService implements Rater<Rate> {
